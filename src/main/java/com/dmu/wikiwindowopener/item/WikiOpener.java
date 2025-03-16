39a1fff8253ec00f7262fcb04a72f8398ecb2d2f
Denise Müller <146842156+denisemueller02@users.noreply.github.com>
2025-03-16 19:22:06 +0100

    Item registered correctly

--- src/main/java/com/dmu/wikiwindowopener/item/WikiOpener.java
+++ src/main/java/com/dmu/wikiwindowopener/item/WikiOpener.java
@@ -4,10 +4,16 @@ import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.UnbreakableComponent;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class WikiOpener extends Item {
     public WikiOpener(Settings settings) {
@@ -18,5 +24,10 @@ public class WikiOpener extends Item {
     public ActionResult use(World world, PlayerEntity user, Hand hand) {
         user.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable("Right Click any Block using this tool to open its minecraft Wiki page!").formatted(Formatting.GOLD));
     }
 }
