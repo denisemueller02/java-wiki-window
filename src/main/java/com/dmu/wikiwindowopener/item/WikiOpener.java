1be2f56bde9ab4e2af03dd82b02cef7b96cab1f2
Denise Müller <146842156+denisemueller02@users.noreply.github.com>
2025-04-08 17:38:35 +0200

    Raycast to get item for specific wiki page

--- src/main/java/com/dmu/wikiwindowopener/item/WikiOpener.java
+++ src/main/java/com/dmu/wikiwindowopener/item/WikiOpener.java
@@ -28,19 +28,53 @@ public class WikiOpener extends Item {
         super(settings);
         settings.component(DataComponentTypes.UNBREAKABLE, new UnbreakableComponent(true));
     }
+
+
     @Override
     public ActionResult use(World world, PlayerEntity user, Hand hand) {
         if (world.isClient()) {
             user.playSound(SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, 1.0F, 1.0F);
 
-            String url = "https://minecraft.wiki/";
             MinecraftClient client = MinecraftClient.getInstance();
-            Util.getOperatingSystem().open(url);
-            
+
+            // Perform a raycast to find the block the player is looking at
+            var hitResult = client.crosshairTarget;
+            if (hitResult != null && hitResult.getType() == net.minecraft.util.hit.HitResult.Type.BLOCK) {
+                var blockHitResult = (net.minecraft.util.hit.BlockHitResult) hitResult;
+                var pos = blockHitResult.getBlockPos();
+                var state = world.getBlockState(pos);
+                var block = state.getBlock();
+                var id = net.minecraft.registry.Registries.BLOCK.getId(block);
+
+                // Build the URL using the block ID (e.g. "minecraft:stone" → "Stone")
+                String blockName = id.getPath().replace('_', ' ');
+                blockName = capitalizeWords(blockName);
+                String url = "https://minecraft.wiki/wiki/" + blockName;
+
+                Util.getOperatingSystem().open(url);
+            } else {
+                // Default wiki homepage if nothing is hit
+                Util.getOperatingSystem().open("https://minecraft.wiki/");
+            }
         }
 
         return ActionResult.SUCCESS;
     }
+
+    private String capitalizeWords(String input) {
+        String[] words = input.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                sb.append(Character.toUpperCase(word.charAt(0)))
+                        .append(word.substring(1))
+                        .append("_");  // Use underscore instead of space for actual Wiki URL
+            }
+        }
+        return sb.toString().replaceAll("_$", ""); // Remove trailing underscore
+    }
+
+
 
 
 
