f2a770a49dd86b76636b793b4bf8f1c66417a142
Denise Müller <146842156+denisemueller02@users.noreply.github.com>
2025-04-08 17:10:28 +0200

    Wiki Chat Message

--- src/main/java/com/dmu/wikiwindowopener/item/WikiOpener.java
+++ src/main/java/com/dmu/wikiwindowopener/item/WikiOpener.java
@@ -2,6 +2,7 @@ package com.dmu.wikiwindowopener.item;
 
 import com.dmu.wikiwindowopener.screens.WikiScreen;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.navigation.GuiNavigation;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.UnbreakableComponent;
 import net.minecraft.entity.player.PlayerEntity;
@@ -15,6 +16,10 @@ import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 public class WikiOpener extends Item {
@@ -24,12 +29,29 @@ public class WikiOpener extends Item {
     }
     @Override
     public ActionResult use(World world, PlayerEntity user, Hand hand) {
-        user.playSound(SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, 1.0F, 1.0F);
-        MinecraftClient.getInstance().setScreen(
-                new WikiScreen(Text.empty())
-        );
+        if (world.isClient()) {
+            // Create a clickable chat message with a link
+            Text linkText = Text.literal("Click here to open the Minecraft Wiki")
+                    .styled(style -> style
+                            .withColor(Formatting.AQUA)
+                            .withUnderline(true)
+                            .withClickEvent(new net.minecraft.text.ClickEvent(
+                                    net.minecraft.text.ClickEvent.Action.OPEN_URL,
+                                    "https://minecraft.wiki/"
+                            ))
+                            .withHoverEvent(new net.minecraft.text.HoverEvent(
+                                    net.minecraft.text.HoverEvent.Action.SHOW_TEXT,
+                                    Text.literal("Opens the official Minecraft Wiki")
+                            ))
+                    );
+
+            user.sendMessage(linkText, false); // false: not an action bar message
+            user.playSound(SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, 1.0F, 1.0F);
+        }
+
         return ActionResult.SUCCESS;
     }
+
 
     @Override
     public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
